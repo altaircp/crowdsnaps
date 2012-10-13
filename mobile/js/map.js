7078859b92c25d8a4d6ef30833be4b0aab429bf8
noreen daypuyat <n.daypuyat@gmail.com>
2012-10-13 22:26:41 +0800

    map update

---
+++ mobile/js/map.js
@@ -0,0 +1,16 @@
+function getMap() {
+    //var myLatlng = new google.maps.LatLng(window.localStorage.getItem("latitude"), window.localStorage.getItem("longitude"));
+    //alert(window.localStorage.getItem("latitude"));
+    //alert(window.localStorage.getItem("longitude"));
+   
+    $('#map-canvas').toggle(); 
+    var myLatlng = new google.maps.LatLng(14.554729,121.0244452);
+
+    var myOptions = {
+        zoom: 18,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+}

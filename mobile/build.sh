2bb9e1823274b43b9d122d34b1e2535959143965
Altair <altaircp@gmail.com>
2012-10-13 21:59:13 +0800

    updates

--- mobile/build.sh
+++ mobile/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 java -jar yuicompressor-2.4.7.jar --type js js/webworks-1.0.2.9.js > js/application-min.js;
+java -jar yuicompressor-2.4.7.jar --type js js/bbui-0.9.4-min.js >> js/application-min.js;
 java -jar yuicompressor-2.4.7.jar --type js js/jquery-1.8.1.min.js >> js/application-min.js;
 java -jar yuicompressor-2.4.7.jar --type js js/main.js >> js/application-min.js;
 java -jar yuicompressor-2.4.7.jar --type css css/bbui-0.9.4.css > css/application-min.css;
